bae3172651af86b6a41a2c231ec2f09dfa6996e0
JonathasGit <jonathas-25@hotmail.com>
2022-03-10 09:22:24 -0300

    #4 código da aula 4

---
+++ src/js/js.js
@@ -0,0 +1,9 @@
+function start() { /*Comandos mudam porque está usando Jquery*/ 
+
+$("#inicio").hide(); //Ocultar a div inicio
+
+$("#fundoGame").append("<div id='jogador'></div>");
+$("#fundoGame").append("<div id='inimigo1'></div>");
+$("#fundoGame").append("<div id='inimigo2'></div>");
+$("#fundoGame").append("<div id='amigo'></div>");
+}
