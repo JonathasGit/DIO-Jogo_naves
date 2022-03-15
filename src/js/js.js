008e6ca16159dc73a82d4949f049817e78bcb387
JonathasGit <jonathas-25@hotmail.com>
2022-03-15 08:11:17 -0300

    #5 primeira e segunda animação

--- src/js/js.js
+++ src/js/js.js
@@ -2,8 +2,8 @@ function start() { /*Comandos mudam porque está usando Jquery*/
 
 $("#inicio").hide(); //Ocultar a div inicio
 
-$("#fundoGame").append("<div id='jogador'></div>");
-$("#fundoGame").append("<div id='inimigo1'></div>");
+$("#fundoGame").append("<div id='jogador' class='anima1'></div>"); /*Para fazer o movimento no helicoptero amigo*/ 
+$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 $("#fundoGame").append("<div id='inimigo2'></div>");
 $("#fundoGame").append("<div id='amigo'></div>");
 }
