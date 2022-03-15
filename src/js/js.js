1aae023fffbbcc71e66a4e41ce38941ff25a5e6e
JonathasGit <jonathas-25@hotmail.com>
2022-03-15 09:10:42 -0300

    #6 código da aula toda

--- src/js/js.js
+++ src/js/js.js
@@ -6,4 +6,30 @@ $("#fundoGame").append("<div id='jogador' class='anima1'></div>"); /*Para fazer 
 $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");/*Para fazer o movimento no helicoptero Inimigo*/ 
 $("#fundoGame").append("<div id='inimigo2'></div>");
 $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+
+//Principais variaveis do jogo
+
+var jogo = {}
+
+// Game Loop
+
+jogo.timer = setInterval(loop,30);
+
+function loop() {
+    movefundo();
+}  //fim do loop
+
+// Movendo o fundo
+
+function movefundo() {
+
+    esquerda = parseInt($("#fundoGame").css("background-position"));
+    $("#fundoGame").css("background-position",esquerda-1);
 }
+
+
+
+
+
+} // fim do jogo 
