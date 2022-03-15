b6aba44fa36b80f08ad09a779617cc424b1f81e5
JonathasGit <jonathas-25@hotmail.com>
2022-03-15 08:56:34 -0300

    #5 código da aula toda

--- src/js/js.js
+++ src/js/js.js
@@ -3,7 +3,7 @@ function start() { /*Comandos mudam porque está usando Jquery*/
 $("#inicio").hide(); //Ocultar a div inicio
 
 $("#fundoGame").append("<div id='jogador' class='anima1'></div>"); /*Para fazer o movimento no helicoptero amigo*/ 
-$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");/*Para fazer o movimento no helicoptero Inimigo*/ 
 $("#fundoGame").append("<div id='inimigo2'></div>");
-$("#fundoGame").append("<div id='amigo'></div>");
+$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 }
