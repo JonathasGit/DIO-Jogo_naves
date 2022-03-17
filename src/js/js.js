bc37397ba8808e25258097467d6539573d8b0027
JonathasGit <jonathas-25@hotmail.com>
2022-03-16 21:57:40 -0300

    #14 criando explosao

--- src/js/js.js
+++ src/js/js.js
@@ -41,6 +41,8 @@ function start() { /*Comandos mudam porque está usando Jquery*/
     jogo.timer = setInterval(loop, 30);
 
     function loop() {
+
+
         movefundo();
         movejogador();
         moveinimigo1();
@@ -185,11 +187,64 @@ function start() { /*Comandos mudam porque está usando Jquery*/
     }
 
     function colisao(){
+
         var colisao1 = ($("#jogador").collision($("#inimigo1"))); // colisão do jogador com inimigo1
 
-        console.log(colisao1);
+        if (colisao1.length>0){ //identificando se a var é  maior que zero 
 
+            //posição da explosao        
+            inimigo1X = parseInt($("#inimigo1").css("left"));
+            inimigo1Y = parseInt($("#inimigo1").css("top"));
+            explosao1(inimigo1X,inimigo1Y);
+
+
+
+
+            // Caso haja colisão, colocar o inimigo1 no lugar dele
+            posicaoY = parseInt(Math.random() * 334); // reposicionar inimigo
+            $("#inimigo1").css("left",694);
+            $("#inimigo1").css("top",posicaoY);
+        }
     }
+
+    /*
+     Código do professor
+    function colisao() {
+        var colisao1 = ($("#jogador").collision($("#inimigo1")));
+        // jogador com o inimigo1
+            
+            if (colisao1.length>0) {
+                
+            inimigo1X = parseInt($("#inimigo1").css("left"));
+            inimigo1Y = parseInt($("#inimigo1").css("top"));
+            explosao1(inimigo1X,inimigo1Y);
+        
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left",694);
+            $("#inimigo1").css("top",posicaoY);
+            }
+        
+        }*/
+
+    function explosao1(inimigo1X,inimigo1Y) {
+        $("#fundoGame").append("<div id='explosao1'></div"); // criando a div 
+        $("#explosao1").css("background-image", "url(imgs/explosao.png)");  //tern a imagem 
+        var div=$("#explosao1");
+        div.css("top", inimigo1Y); // onde vai surgir a explosão 
+        div.css("left", inimigo1X);
+        div.animate({width:200, opacity:0}, "slow"); // até 200 a div vai crescer
+        
+        var tempoExplosao=window.setInterval(removeExplosao, 1000); // para remover a explosão 
+        
+            function removeExplosao() {
+                
+                div.remove();
+                window.clearInterval(tempoExplosao);
+                tempoExplosao=null;
+                
+            }
+            
+        } 
 
 
 
